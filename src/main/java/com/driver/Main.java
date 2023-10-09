79ed20c1dbfbb1511c26521e75f9b3c838f10454
ss9324558 <ss9324558@gmail.com>
2023-10-09 21:55:09 +0530

    done11

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,11 +2,15 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
+//        RWOnly obj=new RWOnly();
+////        obj.name="Suraj";
+////        name has private access in RWOnly
+////        System.out.println(name);
+//        obj.setName("Suraj");
+//        obj.getName();
         RWOnly obj=new RWOnly();
-//        obj.name="Suraj";
-//        name has private access in RWOnly
-//        System.out.println(name);
-        obj.setName("Suraj");
-        obj.getName();
+        System.out.println(obj.getName());
+        obj.setName("ram");
+        System.out.println(obj.getName());
     }
 }
